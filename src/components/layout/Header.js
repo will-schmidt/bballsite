98c87ce6db405c9573bd37eff5891a0c551ce688
Will Schmidt <will.schmidt@gmail.com>
2020-01-08 17:10:09 +1300

    Fix up styling of nav and add fontawesome icon to logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,19 +3,22 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink,
   Link
 } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketballBall } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   return (
     <header>
       <div style={headerStyleContainer} className="container">
         <Link to='/' style={logoStyle}>
-          BBall Data
+        <FontAwesomeIcon icon={faBasketballBall} />BBall Data
         </Link>
-        <ul style={navbar} className="header-nav">
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
+        <ul className="header-nav">
+          <li><NavLink to='/' activeClassName="active">Home</NavLink></li>
+          <li><NavLink to='/about' activeClassName="active">About</NavLink></li>
         </ul>
       </div>
     </header>
@@ -36,8 +39,4 @@ const headerStyleContainer = {
   padding: '1rem 1rem',
   display: 'flex',
   alignItems: 'flex-end'
-}
-
-const navbar = {
-  marginleft: '1rem'
 }
