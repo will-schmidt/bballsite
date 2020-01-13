978ea1ea784f322973263e9445e8da12bb887bcd
Will Schmidt <will.schmidt@gmail.com>
2020-01-13 17:04:21 +1300

    Add more to team page

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Moment from 'react-moment'
 import axios from 'axios'
 import './team.css'
 
@@ -15,6 +16,11 @@ export default class Team extends Component {
       team: teamResponse.data
     })
 
+    const dateTo = Moment().format('YYYY-MM-DD')
+ 
+
+    console.log(`Date... ${dateTo}` )
+
     const gamesResponse = await axios(`https://www.balldontlie.io/api/v1/games?team_ids[]=${this.props.match.params.teamId}&start_date=2019-12-08&end_date=2019-12-19`)
     console.log(gamesResponse.data)
 
@@ -24,15 +30,19 @@ export default class Team extends Component {
   }
   
   render() {
+    
     if(!this.state.team) return null
     return (
+      
       <section className="Team container" style={IndividualTeam}>
+        
         <div className='team-img-wrapper' >
           <img src={`http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/${(this.state.team.abbreviation).toLowerCase()}.png`} alt={this.state.team.full_name}  />
         </div>
        
         <div style={TeamDetails}>
           <h1>{this.state.team.full_name}</h1>
+          <Moment format="YYYY/MM/DD">{this.props.dateToFormat}</Moment>
           <ul>
             <li><strong>{this.state.team.division} Division</strong></li>
             <li>{this.state.team.conference}ern Conference</li>
@@ -41,7 +51,7 @@ export default class Team extends Component {
         </div>
         <div>
           <ul>
-          {this.state.games.map( (game, i) => {
+          {this.state.games.map( (game) => {
 
             return (
               <div key={game.id}>
@@ -51,7 +61,7 @@ export default class Team extends Component {
                 <li >{game.date}</li>
                 <li >{game.home_team_score} - {game.visitor_team_score}</li>
                 </ul>
-              </div>
+              </div>             
             )
           })}
           </ul>
