5150f1c85cbfbe0a20da1d30b4b1cb9dcef2e397
Cristian Florea <cristianflorea1110@yahoo.com>
2020-01-17 23:28:31 +0100

    mentoring

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,42 +1,75 @@
-import React from 'react'
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink,
   Link
-} from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBasketballBall } from '@fortawesome/free-solid-svg-icons'
+} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBasketballBall } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
-export default function Header() {
-  return (
-    <header>
-      <div style={headerStyleContainer} className="container">
-        <Link to='/' style={logoStyle}>
-        <FontAwesomeIcon icon={faBasketballBall} />BBall Data
-        </Link>
-        <ul className="header-nav">
-          <li><NavLink to='/' exact activeClassName="active">Home</NavLink></li>
-          <li><NavLink to='/about' activeClassName="active">About</NavLink></li>
-        </ul>
-      </div>
-    </header>
-  )
+// lodash DEBOUNCE
+
+export default class Header extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const response = await axios.get(
+      `https://www.balldontlie.io/api/v1/players?search=${this.state.value}`
+    );
+    console.log(response);
+  };
+
+  render() {
+    console.log(this.state.value);
+    return (
+      <header>
+        <div style={headerStyleContainer} className="container">
+          <Link to="/" style={logoStyle}>
+            <FontAwesomeIcon icon={faBasketballBall} />
+            BBall Data
+          </Link>
+          <ul className="header-nav">
+            <li>
+              <NavLink to="/" exact activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="active">
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <input onChange={this.handleChange} />
+        <button onClick={this.onSubmit}>Get player</button>
+      </header>
+    );
+  }
 }
 
 const logoStyle = {
-  fontWeight: 'bold',
+  fontWeight: "bold",
   fontFamily: "'Montserrat', 'sans-serif'",
-  color: '#000',
-  textDecoration: 'none',
-  fontSize: '2rem',
-  letterSpacing: '-0.2rem'
-}
+  color: "#000",
+  textDecoration: "none",
+  fontSize: "2rem",
+  letterSpacing: "-0.2rem"
+};
 
 const headerStyleContainer = {
-  borderBottom: '2px solid #000',
-  padding: '1rem 1rem',
-  display: 'flex',
-  alignItems: 'flex-end'
-}
+  borderBottom: "2px solid #000",
+  padding: "1rem 1rem",
+  display: "flex",
+  alignItems: "flex-end"
+};
--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,118 +1,131 @@
-import React, { Component } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
-import axios from 'axios'
-import moment from 'moment'
-import './team.css'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+import "./team.css";
+
+const baseUrl = "https://www.balldontlie.io/api/v1";
 
 export default class Team extends Component {
   state = {
     team: null,
     games: []
-  }
+  };
 
-  async componentDidMount(){
-    const teamResponse = await axios(`https://www.balldontlie.io/api/v1/teams/${this.props.match.params.teamId}`)
-    // console.log('found player', response.data)
-    this.setState({
-      team: teamResponse.data
-    })
-
-    const dateTo = moment().format('YYYY-MM-DD')
-    const dateFrom = moment().subtract(10,'d').format('YYYY-MM-DD')
- 
-
-    const gamesResponse = await axios(`https://www.balldontlie.io/api/v1/games?team_ids[]=${this.props.match.params.teamId}&start_date=${dateFrom}&end_date=${dateTo}`)
-    console.log(gamesResponse.data)
+  fetchData = async () => {
+    const { teamId } = this.props.match.params;
+    const format = "YYYY-MM-DD";
+    const dateTo = moment().format(format);
+    const dateFrom = moment()
+      .subtract(10, "d")
+      .format(format);
+    const teamResponse = await axios(`${baseUrl}/teams/${teamId}`);
+    const gamesResponse = await axios(
+      `${baseUrl}/games?team_ids[]=${teamId}&start_date=${dateFrom}&end_date=${dateTo}`
+    );
 
     this.setState({
+      team: teamResponse.data,
       games: gamesResponse.data.data
-    })
+    });
+  };
 
-    
+  componentDidMount() {
+    this.fetchData();
   }
-  
-  
-  
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.match.params.teamId !== this.props.match.params.teamId) {
+      this.fetchData();
+    }
+  }
+
   render() {
-    
+    // TODO: research sort function(by date using moment)
+    const { teamId } = this.props.match.params;
+    const { team } = this.state;
+    if (!this.state.team) return null;
 
-
-    if(!this.state.team) return null
     return (
-      
       <section className="Team container" style={IndividualTeam}>
-        
-        <div className='team-img-wrapper' >
-          <img src={`http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/${(this.state.team.abbreviation).toLowerCase()}.png`} alt={this.state.team.full_name}  />
+        <div className="team-img-wrapper">
+          <img
+            src={`http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/${this.state.team.abbreviation.toLowerCase()}.png`}
+            alt={this.state.team.full_name}
+          />
         </div>
-       
+
         <div style={TeamDetails}>
           <h1>{this.state.team.full_name}</h1>
-          
+
           <ul>
-            <li><strong>{this.state.team.division} Division</strong></li>
+            <li>
+              <strong>{this.state.team.division} Division</strong>
+            </li>
             <li>{this.state.team.conference}ern Conference</li>
           </ul>
-
         </div>
         <div>
           <ul>
-          {this.state.games.map( (game) => {
-              let isHomeTeam
-              let wonGame
-              if (game.home_team.id === this.props.match.params.teamId) {
-                isHomeTeam = true
-                if (game.home_team_score > game.visitor_team_score) {
-                  wonGame = true
-                } else {
-                  wonGame = false
-                }
-              } else {
-                isHomeTeam = false
-                if (game.visitor_team_score > game.home_team_score) {
-                  wonGame = true
-                } else {
-                  wonGame = false
-                }
-              }
-            
-            return (
-              <div key={game.id}>
- 
-                <h3><Link exact to={`/team/${game.home_team.id}`}>{game.home_team.full_name}</Link> vs <Link to={`/team/${game.visitor_team.id}`}>{game.visitor_team.full_name}</Link></h3>
-                
-                <ul>
-                  <li>{wonGame ? `<span class="won">Won</span>` : `<span class="lost">Lost</span>`}</li>
-                  <li>{isHomeTeam ? 'Home game' : 'Away game'}</li>
-                <li >{game.date}></li>
-                <li >{game.home_team_score} - {game.visitor_team_score}</li>
-                </ul>
-              </div>             
-            )
-          })}
+            {this.state.games.map(game => {
+              const isHomeTeam = game.home_team.id === parseInt(teamId);
+              const pageTeamScore = isHomeTeam
+                ? game.home_team_score
+                : game.visitor_team_score;
+              const oppositeTeamScore = !isHomeTeam
+                ? game.home_team_score
+                : game.visitor_team_score;
+              const wonGame = pageTeamScore > oppositeTeamScore;
+              const inProgress = pageTeamScore === 0 && oppositeTeamScore === 0;
+
+              return (
+                <div key={game.id}>
+                  <h3>
+                    <Link exact to={`/team/${game.home_team.id}`}>
+                      {game.home_team.full_name}
+                    </Link>
+                    vs
+                    <Link to={`/team/${game.visitor_team.id}`}>
+                      {game.visitor_team.full_name}
+                    </Link>
+                  </h3>
+
+                  <ul>
+                    {inProgress ? (
+                      <li>In progress or Scheduled</li>
+                    ) : (
+                      <li>
+                        <span class={wonGame ? "won" : "lost"}>
+                          {wonGame ? "Won" : "Lost"}
+                        </span>
+                      </li>
+                    )}
+
+                    <li>{isHomeTeam ? "Home game" : "Away game"}</li>
+                    <li>{game.date}></li>
+                    <li>
+                      {game.home_team_score} - {game.visitor_team_score}
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
           </ul>
         </div>
-        </section>
-
-    )
+      </section>
+    );
   }
 }
 
-
 const IndividualTeam = {
-  padding: '2rem 1rem',
-  display: 'flex'
-}
+  padding: "2rem 1rem",
+  display: "flex"
+};
 
 // const TeamLogo = {
 //   maxWidth: '200px'
 // }
 
 const TeamDetails = {
-  paddingTop: '2rem'
-}
+  paddingTop: "2rem"
+};
